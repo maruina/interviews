1974a25f01aa53068157a793a856c3662b9e5c8f
ruio <matteo.ruina@gmail.com>
2013-11-09 14:43:50 +0100

    GCD(a,b) returns the Greatest common divisor between a and b

--- scripts/GCD.py
+++ scripts/GCD.py
@@ -7,20 +7,24 @@ import sys
 import math
 
 def GCD(a,b):
-    int major = 0
-    int minor = 0
+    int maggiore = 0
+    int minore = 0
     #Check if a and b are positive integers
-    if a== 0 or b == 0:
-        print "GCD(%d,%d) is 0" % (a, b)
+    
+    #
+    if a == 0:
+        print "GCD(0,%d) is %d" % (b, b)
+    if b == 0:
+        print "GCD(%d,0) is %d" % (a, a)
     if a == b:
         print "GCD(%d,%d) is %d" % (a, b, a)
-        sys.exit
+        sys.exit(2)
     if a < b:
-        major = b
-        minor = a
+        maggiore = b
+        minore = a
     else:
-        major = a
-        minor = b
+        maggiore = a
+        minore = b
     
 
 def main():

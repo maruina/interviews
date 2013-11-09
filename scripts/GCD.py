5fe3092d9dd0bbe818e75f92c4ce8e5da72ea806
ruio <matteo.ruina@gmail.com>
2013-11-09 13:53:19 +0100

    GCD(a,b) returns the Greatest common divisor between a and b

---
+++ scripts/GCD.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+#
+# GCD(a,b) returns the Greatest common divisor between a and b
+#
+
+def GCD(a,b):
+    #Check if a and b are positive integers
+    
+    if a == b:
+        print "GCD(%d,%d) is %d" % (a, b, a)
+        sys.exit
+    if a < b:
+        max = b
+        min = a
+    else:
+        
+
+def main():
+    
+
+
+if __name__ == '__main__':
+    main()
